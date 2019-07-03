37fba2bd3e525ebc308e94933c1e556e265eac4f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-03 11:17:08 -0500

    set up server

---
+++ controller/home.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+
+exports.index = async (req, res) => {
+  console.log('req');
+}
--- server.js
+++ server.js
@@ -9,4 +9,10 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use('/', home);
+
 const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is up on port: ${port}`);
+}) 
