7ae3b8813d68f8d6a54ce2fbbd8933b48b302bab
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-03 14:59:26 -0500

    set up server

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
+import socketIOClient from "socket.io-client";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  componentDidMount() {
+    // socket.emit('join', {}, (error) => {
+    //   if (error) {
+    //     alert(error);
+    //   }
+    // });
+    const socket = socketIOClient('http://localhost:5000/');
+    socket.on('join', 'test');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
