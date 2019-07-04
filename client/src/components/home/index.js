2c872861a13a1b36ae6f98e5f93030fc5d7072c9
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-04 07:35:56 -0500

    create home page

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -2,19 +2,34 @@ import React, {useState} from 'react';
 
 function Home() {
   const [roomText, setRoomText] = useState("");
+  const [idText, setIdText] = useState("");
   const onSubmit = e => {
     e.preventDefault();
   };
 
+  const onEnter = e => {
+    e.preventDefault();
+  }
+
   return (
     <section className='app'>
       <section className='form-container enter-room'>
+        <form onSubmit={onEnter} >
+          <input 
+            type='text'
+            placeholder='Enter Room Key'
+            value={idText} 
+            onChange={(e) => setIdText(e.target.value)} 
+          />
 
+          <input type='Enter' />
+        </form>
       </section>
+
       <section className='form-container create-room'>
         <form onSubmit={onSubmit} >
           <input 
-            type='text' 
+            type='text'
             placeholder='Game Room'
             value={roomText} 
             onChange={(e) => setRoomText(e.target.value)} 
