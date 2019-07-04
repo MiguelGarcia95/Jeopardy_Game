825a3145d16222396f43ca5ee62738a9298f0a63
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-03 22:30:22 -0500

    create home page

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -2,11 +2,24 @@ import React, {useState} from 'react';
 
 function Index() {
   const [roomText, setRoomText] = useState("");
+  const onSubmit = e => {
+    e.preventDefault();
+  };
 
   return (
     <section className='app'>
-      {roomText}
-      <input placeholder='Game Room' onChange={(e) => setRoomText(e.target.value)} />
+      <section className='form-container'>
+        <form onSubmit={onSubmit} >
+          <input 
+            type='text' 
+            placeholder='Game Room'
+            value={roomText} 
+            onChange={(e) => setRoomText(e.target.value)} 
+          />
+          
+          <input type='submit' />
+        </form>
+      </section>
     </section>
   )
 }
