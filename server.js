7e4c02f2e85f81a97495f7eb2da5676cc7a45ef3
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-04 19:20:29 -0500

    send home data to socket

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const home = require('./routes/home');
 const app = express();
+require('./db/mongoose');
 
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
@@ -21,11 +22,11 @@ io.on('connection', socket => {
   socket.on('createRoom', (room, callback) => {
     console.log(room);
     console.log('id: ', socket.id)
-
   })
 
   socket.on("disconnect", () => console.log("Client Disconnected"));
 })
+
 
 
 server.listen(port, () => {
